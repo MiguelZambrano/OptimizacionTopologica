ef9089f525368bd0782094ff150526ba9cb8437a
Miguel Zambrano <mazambranoga@unal.edu.co>
2019-04-29 11:54:29 -0500

    Updating heat example

--- FEM/heat2D.m
+++ FEM/heat2D.m
@@ -1,6 +1,6 @@
 %% STATIONARY HEAT CONDUCTION, SQUARES 2D, VECTORIZED %%
-nelx = 40;
-nely = 30;
+nelx = 100;
+nely = 100;
 %% PREPARE FINITE ELEMENT ANALYSIS
 KE = [4 -1 -2 -1; -1 4 -1 -2; -2 -1 4 -1; -1 -2 -1 4];
 nodenrs = reshape(1:(1+nelx)*(1+nely),1+nely,1+nelx);
@@ -9,10 +9,11 @@ edofMat = repmat(edofVec,1,4)+repmat([0 nely+1 nely -1],nelx*nely,1);
 iK = reshape(kron(edofMat,ones(4,1))',16*nelx*nely,1);
 jK = reshape(kron(edofMat,ones(1,4))',16*nelx*nely,1);
 % DEFINE LOADS AND SUPPORTS
-%F = 10*ones((nely+1)*(nelx+1),1);
+F = ones((nely+1)*(nelx+1),1);
+fixeddofs = union([1:nely+1,1:nely+1:(nelx+1)*(nely+1),nely+1:nely+1:(nelx+1)*(nely+1)],(nely+1)*(nelx)+1:(nelx+1)*(nely+1));
 %fixeddofs = (nelx+1)*(nely+1)-nely:1:(nelx+1)*(nely+1);
-F = sparse(floor(0.5*(nelx+1)*(nely+1))+1,1,1,(nely+1)*(nelx+1),1);
-fixeddofs = (nely+1)*(ceil(2*(nelx+1)/5)-1)+1:nely+1:(nely+1)*(ceil(3*(nelx+1)/5)-1)+1;
+%F = sparse(floor(0.5*(nelx+1)*(nely+1))+1,1,1,(nely+1)*(nelx+1),1);
+%fixeddofs = (nely+1)*(ceil(2*(nelx+1)/5)-1)+1:nely+1:(nely+1)*(ceil(3*(nelx+1)/5)-1)+1;
 alldofs = 1:(nely+1)*(nelx+1);
 freedofs = setdiff(alldofs,fixeddofs);
 U = zeros((nelx+1)*(nely+1),1);
